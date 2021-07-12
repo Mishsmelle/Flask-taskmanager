4103800c7be030781a9dd09e9675b4db2ab94c13
Mishsmelle <michelle.oconnor@shopify.com>
2021-07-12 16:16:59 +0000

    Bind data from db to accordian

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,28 @@
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
 $(document).ready(function () {
-    $('.sidenav').sidenav({
+    $(".sidenav").sidenav({
         edge: "right"
     });
-    $('.collapsible').collapsible();
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
+
+
+
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+//     let tooltips = document.querySelectorAll(".tooltipped");
+//     let tooltipsInstance = M.Tooltip.init(tooltips);
+// });
